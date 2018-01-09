383b68606dba12584ed844d03a1aa2636b0b2cdb
Udayraj123 <rhuge123@gmail.com>
2018-01-09 12:17:09 +0700

    update

--- practice/NewLearns.cpp
+++ practice/NewLearns.cpp
@@ -1,3 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define MAX 1000000000
+
+#define vi vector<ll> 
+#define vii vector< vi > 
+
+#ifdef ONLINE_JUDGE
+	#define ONLINE_JUDGE_DEF true
+#else
+	#define ONLINE_JUDGE_DEF false
+#endif
+
+
+#define printa(x,n) for(int i = 0; i < n; ++i)cout<<x[i]<<" ";printf("\n");
+#define DEBUGA(x,n) if(!ONLINE_JUDGE_DEF){for(int i = 0; i < n; ++i)cout<<x[i]<<" ";printf("\n");}
+#define DEBUG(x) if(!ONLINE_JUDGE_DEF){cout << ">> " << #x << " : " << (x) << endl;}
+using namespace std;
 
 // Catalan's Number -
 answer to 3 eqv problems :
